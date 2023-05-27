47b3e5a9b5b0e9fce4230643513d433fa4a741a5
Paras Saxena <85274192+parassaxena06@users.noreply.github.com>
2023-05-27 10:42:19 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p>© 2023 Airbnb clone! All rights reserved - this is a React.js Project! </p>
+            <p> Privacy · Terms · Conditions · Company Details </p>
+    </div>
+  )
+}
+
+export default Footer;
